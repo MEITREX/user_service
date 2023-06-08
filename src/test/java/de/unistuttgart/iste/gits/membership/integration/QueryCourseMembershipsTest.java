43a3251d81b46afbbeae2e97c16221a96431f542
Timothy <tidee98@web.de>
2023-06-08 12:22:48 +0200

    adding integration test

---
+++ src/test/java/de/unistuttgart/iste/gits/membership/integration/QueryCourseMembershipsTest.java
@@ -0,0 +1,36 @@
+package de.unistuttgart.iste.gits.membership.integration;
+
+import de.unistuttgart.iste.gits.generated.dto.CourseMembershipDto;
+import de.unistuttgart.iste.gits.membership.persistence.repository.CourseMembershipRepository;
+import de.unistuttgart.iste.gits.util.GraphQlApiTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.test.tester.GraphQlTester;
+
+import java.util.UUID;
+
+@GraphQlApiTest
+public class QueryCourseMembershipsTest {
+
+    @Autowired
+    private CourseMembershipRepository membershipRepository;
+
+    @Test
+    void testNoMembershipExisting(GraphQlTester tester){
+        //GraphQL query
+        String query = """
+                query {
+                    courseMemberships(id: "%s") {
+                        userId
+                        courseId
+                        role
+                    }
+                }
+                """.formatted(UUID.randomUUID());
+        tester.document(query)
+                .execute()
+                .path("courseMemberships")
+                .entityList(CourseMembershipDto.class)
+                .hasSize(0);
+    }
+}
