5c2ec423978b65f2c15be070ef58c901f8394b47
myluki2000 <lukas636@live.com>
2023-09-16 18:41:54 +0200

    SubscriptionController: inject membership service

--- src/main/java/de/unistuttgart/iste/gits/user_service/controller/SubscriptionController.java
+++ src/main/java/de/unistuttgart/iste/gits/user_service/controller/SubscriptionController.java
@@ -4,7 +4,9 @@ import de.unistuttgart.iste.gits.common.event.CourseChangeEvent;
 import de.unistuttgart.iste.gits.user_service.service.MembershipService;
 import io.dapr.Topic;
 import io.dapr.client.domain.CloudEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +14,10 @@ import reactor.core.publisher.Mono;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class SubscriptionController {
 
-    MembershipService membershipService;
+    private final MembershipService membershipService;
 
     @Topic(name = "course-changes", pubsubName = "gits")
     @PostMapping(path = "/user-service/course-changes-pubsub")
