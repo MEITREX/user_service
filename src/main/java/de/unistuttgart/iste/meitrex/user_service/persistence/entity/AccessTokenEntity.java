248c973b406935dec49cfad9af4ed85a801c5b22
Mykhailo Zelia <mishazelea@gmail.com>
2025-02-21 16:46:25 +0100

    Change AccessTokenEntity

--- src/main/java/de/unistuttgart/iste/meitrex/user_service/persistence/entity/AccessTokenEntity.java
+++ src/main/java/de/unistuttgart/iste/meitrex/user_service/persistence/entity/AccessTokenEntity.java
@@ -54,6 +54,6 @@ public class AccessTokenEntity {
     @Column(nullable = false)
     private OffsetDateTime accessTokenExpiresAt;
 
-    @Column(updatable = false)
+    @Column
     private OffsetDateTime refreshTokenExpiresAt;
 }
