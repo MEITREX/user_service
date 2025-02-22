f3edf9aca2d3b81f1d8ad918d503b2fd8b775fb4
Mykhailo Zelia <mishazelea@gmail.com>
2025-02-22 13:34:32 +0100

    Add providers config

---
+++ src/main/java/de/unistuttgart/iste/meitrex/user_service/config/ExternalServiceProviderConfiguration.java
@@ -0,0 +1,15 @@
+package de.unistuttgart.iste.meitrex.user_service.config;
+
+import de.unistuttgart.iste.meitrex.user_service.persistence.entity.ExternalServiceProvider;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "thirdparty")
+public class ExternalServiceProviderConfiguration {
+    private Map<ExternalServiceProvider, ExternalServiceProviderInfo> providers;
+}
---
+++ src/main/java/de/unistuttgart/iste/meitrex/user_service/config/ExternalServiceProviderInfo.java
@@ -0,0 +1,25 @@
+package de.unistuttgart.iste.meitrex.user_service.config;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+/**
+ * Configuration class for an external service provider's OAuth2 authentication details.
+ * This class holds credentials and endpoint URLs required for obtaining and refreshing access tokens.
+ */
+@Data
+@Builder
+public class ExternalServiceProviderInfo {
+    @NonNull
+    private final String clientId;
+
+    @NonNull
+    private final String clientSecret;
+
+    @NonNull
+    private final String tokenRequestUrl;
+
+    private final String refreshTokenUrl;
+}
+
