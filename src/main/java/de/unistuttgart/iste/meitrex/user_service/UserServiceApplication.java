09ab681ff75e62dcf83a88412e39bccb5a1638b0
Mykhailo Zelia <mishazelea@gmail.com>
2025-02-23 15:32:16 +0100

    Fix NoSuchBeanException

--- src/main/java/de/unistuttgart/iste/meitrex/user_service/UserServiceApplication.java
+++ src/main/java/de/unistuttgart/iste/meitrex/user_service/UserServiceApplication.java
@@ -3,8 +3,6 @@ package de.unistuttgart.iste.meitrex.user_service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 import java.util.Arrays;
 
@@ -13,7 +11,7 @@ import java.util.Arrays;
  * <p>
  *
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @Slf4j
 public class UserServiceApplication {
 
