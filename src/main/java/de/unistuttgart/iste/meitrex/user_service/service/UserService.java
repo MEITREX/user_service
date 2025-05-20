2f057aa2b251add34fe41cdcbcfbf92e982169ed
Aaron Schmid <st166450@stud.uni-stuttgart.de>
2025-05-20 17:46:55 +0200

    add database

--- src/main/java/de/unistuttgart/iste/meitrex/user_service/service/UserService.java
+++ src/main/java/de/unistuttgart/iste/meitrex/user_service/service/UserService.java
@@ -4,7 +4,7 @@ import de.unistuttgart.iste.meitrex.common.user_handling.LoggedInUser;
 import de.unistuttgart.iste.meitrex.generated.dto.PublicUserInfo;
 import de.unistuttgart.iste.meitrex.generated.dto.UserInfo;
 import de.unistuttgart.iste.meitrex.user_service.config.KeycloakWrapper;
-import de.unistuttgart.iste.meitrex.user_service.mapper.RealmMapper;
+import de.unistuttgart.iste.meitrex.user_service.persistence.mapper.RealmMapper;
 import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.SelectedField;
 import lombok.RequiredArgsConstructor;
