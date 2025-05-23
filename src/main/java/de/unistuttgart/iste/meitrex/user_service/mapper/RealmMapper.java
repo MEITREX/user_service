660d20612743b83b6ff2301578d1d52d48908c33
c-neli-r <cornelius.rost@web.de>
2025-05-23 14:00:43 +0200

    fix

---
+++ src/main/java/de/unistuttgart/iste/meitrex/user_service/mapper/RealmMapper.java
@@ -0,0 +1,31 @@
+package de.unistuttgart.iste.meitrex.user_service.mapper;
+
+import de.unistuttgart.iste.meitrex.common.user_handling.LoggedInUser;
+import de.unistuttgart.iste.meitrex.generated.dto.GlobalUserRole;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+public class RealmMapper {
+
+    private final ModelMapper modelMapper;
+
+    public RealmMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public List<GlobalUserRole> internalRolesToGraphQlRoles(Set<LoggedInUser.RealmRole> realmRoleSet) {
+        return realmRoleSet.stream().map(role -> modelMapper.map(role, GlobalUserRole.class)).toList();
+    }
+
+    public List<GlobalUserRole> keycloakRolesToGraphQlRoles(List<String> keycloakRoles) {
+        if (keycloakRoles == null) {
+            return Collections.emptyList();
+        }
+
+        Set<LoggedInUser.RealmRole> internalRoles = LoggedInUser.RealmRole.getRolesFromKeycloakRoleList(keycloakRoles);
+        return internalRolesToGraphQlRoles(internalRoles);
+    }
+}
