92a0fb3e8e4c224f51cca8f137d71fa1ac3c8ca6
c-neli-r <cornelius.rost@web.de>
2025-05-25 22:02:02 +0200

    Add additional settings queries

--- src/main/java/de/unistuttgart/iste/meitrex/user_service/controller/SettingsController.java
+++ src/main/java/de/unistuttgart/iste/meitrex/user_service/controller/SettingsController.java
@@ -5,6 +5,7 @@ import de.unistuttgart.iste.meitrex.user_service.service.SettingsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import java.util.UUID;
 
@@ -14,8 +15,19 @@ public class SettingsController {
     private final SettingsService settingsService;
 
     @MutationMapping
-    public Settings saveSettings(@Argument UUID userId, @Argument SettingsInput input) {
-        return settingsService.save(userId,input);
+    public Settings updateSettings(@Argument UUID userId, @Argument SettingsInput input) {
+        return settingsService.update(userId,input);
+    }
+
+    @QueryMapping
+    public Settings findUserSettings(@Argument UUID userId) {
+        return settingsService.find(userId);
+    }
+
+    // Set default settings
+    @MutationMapping
+    public Settings defaultSettings(@Argument UUID userId){
+        return settingsService.setDefault(userId);
     }
 
 }
--- src/main/java/de/unistuttgart/iste/meitrex/user_service/service/SettingsService.java
+++ src/main/java/de/unistuttgart/iste/meitrex/user_service/service/SettingsService.java
@@ -6,6 +6,7 @@ import de.unistuttgart.iste.meitrex.user_service.persistence.entity.Notification
 import de.unistuttgart.iste.meitrex.user_service.persistence.entity.SettingsEntity;
 import de.unistuttgart.iste.meitrex.user_service.persistence.mapper.SettingsMapper;
 import de.unistuttgart.iste.meitrex.user_service.persistence.repository.SettingsRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,23 +21,67 @@ public class SettingsService {
     private final SettingsRepository settingsRepository;
     private final SettingsMapper settingsMapper;
 
-    public Settings save(UUID userId, SettingsInput input) {
+     /**
+     * Update user settings
+     * @param input settings to be updated
+     * @param userId
+     * @return updated user settings
+     */
+    public Settings update(UUID userId, SettingsInput input) {
+        SettingsEntity settings = settingsRepository.findByUserId(userId)
+                .orElseThrow(() -> new EntityNotFoundException("No settings found for user "));
+
+        // Update Notification Settings if present
+        if (settings.getNotification() != null && input.getNotification() != null) {
+            NotificationSettingsEntity notification = settings.getNotification();
+            notification.setGamification(input.getNotification().getGamification());
+            notification.setLecture(input.getNotification().getLecture());
+        }
+
+        // Update Gamification Settings if present
+        if (settings.getGamification() != null && input.getGamification() != null) {
+            GamificationSettingsEntity gamification = settings.getGamification();
+            gamification.setGamification(input.getGamification());
+        }
+
+        // Save and return DTO
+        SettingsEntity saved = settingsRepository.save(settings);
+        return settingsMapper.entityToDto(saved);
+    }
+
+     /**
+     * Return settings of user
+     * @param userId
+     * @return user settings
+     */
+    public Settings find(UUID userId) {
         Optional<SettingsEntity> optional = settingsRepository.findByUserId(userId);
-        SettingsEntity settings = optional.orElseGet(SettingsEntity::new);
-        settings.setUserId(userId);
+        return optional.map(settingsMapper::entityToDto).orElse(null);
+    }
 
-        // Set Notification settings
-        NotificationSettingsEntity notification = Optional.ofNullable(settings.getNotification())
-                .orElseGet(NotificationSettingsEntity::new);
-        notification.setGamification(input.getNotification().getGamification());
-        notification.setLecture(input.getNotification().getLecture());
-        settings.setNotification(notification);
+     /**
+     * Set user settings to default to default settings
+     * @param userId
+     * @return default user settings
+     */
+    public Settings setDefault(UUID userId) {
+        // Get or Create User Settings
+        SettingsEntity settings = settingsRepository.findByUserId(userId)
+                .orElseGet(() -> SettingsEntity.builder().userId(userId).build());
 
-        // Set Gamification Settings
-        GamificationSettingsEntity gamification = Optional.ofNullable(settings.getGamification())
-                        .orElseGet(GamificationSettingsEntity::new);
-        gamification.setGamification(input.getGamification());
-        settings.setGamification(gamification);
+        // Set Default Gamification Settings
+        GamificationSettingsEntity gamificationSettings = GamificationSettingsEntity.builder()
+                .gamification(Gamification.ADAPTIVE_GAMIFICATION_ENABLED)
+                .build();
+
+        // Set Default Notification Settings
+        NotificationSettingsEntity notificationSettings = NotificationSettingsEntity.builder()
+                .gamification(true)
+                .lecture(true)
+                .build();
+
+        settings.setGamification(gamificationSettings);
+        settings.setNotification(notificationSettings);
 
         SettingsEntity saved = settingsRepository.save(settings);
         return settingsMapper.entityToDto(saved);
