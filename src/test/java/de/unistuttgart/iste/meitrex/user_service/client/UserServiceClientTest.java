f2ab54ce981049d865cb6da4e878eb817b21ca00
Mykhailo Zelia <mishazelea@gmail.com>
2025-06-15 14:54:48 +0200

    Test queryAccessToken and getExternalUserIds in UserServiceClientTest

--- src/test/java/de/unistuttgart/iste/meitrex/user_service/client/UserServiceClientTest.java
+++ src/test/java/de/unistuttgart/iste/meitrex/user_service/client/UserServiceClientTest.java
@@ -1,8 +1,14 @@
 package de.unistuttgart.iste.meitrex.user_service.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.unistuttgart.iste.meitrex.common.testutil.GraphQlTesterParameterResolver;
+import de.unistuttgart.iste.meitrex.common.user_handling.LoggedInUser;
+import de.unistuttgart.iste.meitrex.generated.dto.AccessToken;
+import de.unistuttgart.iste.meitrex.generated.dto.ExternalServiceProviderDto;
+import de.unistuttgart.iste.meitrex.generated.dto.ExternalUserIdWithUser;
 import de.unistuttgart.iste.meitrex.generated.dto.UserInfo;
 import de.unistuttgart.iste.meitrex.user_service.exception.UserServiceConnectionException;
+import de.unistuttgart.iste.meitrex.user_service.test_config.MockAccessTokenServiceConfiguration;
 import de.unistuttgart.iste.meitrex.user_service.test_config.MockKeycloakConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +22,7 @@ import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -24,7 +31,7 @@ import static org.hamcrest.Matchers.*;
 
 @ExtendWith(GraphQlTesterParameterResolver.class)
 @SpringBootTest({"spring.main.allow-bean-definition-overriding=true"})
-@ContextConfiguration(classes = MockKeycloakConfiguration.class)
+@ContextConfiguration(classes = {MockKeycloakConfiguration.class, MockAccessTokenServiceConfiguration.class})
 class UserServiceClientTest {
 
     private GraphQlClient graphQlClient;
@@ -116,4 +123,60 @@ class UserServiceClientTest {
 
     }
 
+    @Test
+    void testQueryAccessToken() throws Exception {
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        final LoggedInUser loggedInUser = LoggedInUser.builder()
+                .id(MockKeycloakConfiguration.firstUserId)
+                .userName("firstuser")
+                .firstName("First")
+                .lastName("User")
+                .courseMemberships(List.of())
+                .realmRoles(Set.of())
+                .build();
+
+        final String loggedInUserJson = objectMapper.writeValueAsString(loggedInUser);
+
+        final WebTestClient webTestClientWithHeader = MockMvcWebTestClient
+                .bindToApplicationContext(applicationContext)
+                .configureClient()
+                .baseUrl("/graphql")
+                .defaultHeader("CurrentUser", loggedInUserJson)
+                .build();
+
+        final GraphQlClient customClient = GraphQlClient.builder(new WebTestClientTransport(webTestClientWithHeader)).build();
+        final UserServiceClient userServiceClient = new UserServiceClient(customClient);
+
+        final AccessToken accessToken = userServiceClient.queryAccessToken(loggedInUser, ExternalServiceProviderDto.GITHUB);
+
+        assertThat(accessToken, is(notNullValue()));
+        assertThat(accessToken.getAccessToken(), is("mocked-access-token"));
+        assertThat(accessToken.getExternalUserId(), is(nullValue())); // Adapt if needed
+    }
+
+
+
+    @Test
+    void testQueryExternalUserIds() throws UserServiceConnectionException {
+        final UserServiceClient userServiceClient = new UserServiceClient(graphQlClient);
+
+        List<UUID> userIds = List.of(
+                MockKeycloakConfiguration.firstUserId,
+                MockKeycloakConfiguration.thirdUserId
+        );
+
+        List<ExternalUserIdWithUser> externalIds = userServiceClient.queryExternalUserIds(
+                ExternalServiceProviderDto.GITHUB,
+                userIds
+        );
+
+        assertThat(externalIds, hasSize(2));
+
+        assertThat(externalIds.get(0).getUserId(), is(MockKeycloakConfiguration.firstUserId));
+        assertThat(externalIds.get(0).getExternalUserId(), is("github_firstuser"));
+
+        assertThat(externalIds.get(1).getUserId(), is(MockKeycloakConfiguration.thirdUserId));
+        assertThat(externalIds.get(1).getExternalUserId(), is("github_thirduser"));
+    }
 }
